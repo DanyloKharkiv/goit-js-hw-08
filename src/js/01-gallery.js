044e7988cfa9f844e6e7e88791a47e2c36bd784a
Danylo Kharkiv <donatelod12@gmail.com>
2023-01-16 21:54:22 +0200

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const gallery = document.getElementsByClassName("gallery")[0]
+
+
+createImages(galleryItems)
+
+
+function createImages(e) {
+   const images = e.reduce ((arr,{ preview, original, description }) => {
+       return arr + `<a class="gallery__item" href="${original}"><img class="gallery__image"
+      src="${preview}" alt="${description}"/></a> ` 
+   },"")
+    gallery.insertAdjacentHTML('beforeend', images)    
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionsData: "alt",
+    captionDelay:250,
+});
